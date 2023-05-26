1fa45714537fdc13e0540aa5ad6bf3dc2250ea66
christophersharpe <chrismsharpe123@gmail.com>
2023-05-26 09:55:13 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(logger("dev"));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my Express API template')
+})
+
+
+module.exports = app
