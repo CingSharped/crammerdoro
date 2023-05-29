de725970f89591e47040691b2d0b5b1ac615394f
christophersharpe <chrismsharpe123@gmail.com>
2023-05-29 14:06:13 +0100

    Added authenticator, untested added check for dev to use test db in dev and cramodoro for staging

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = require("./app");
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI, {dbName: 'cramodoro'})
+mongoose.connect(process.env.MONGO_URI, process.env.DEV ? { dbName: 'test' } : {dbName: 'cramodoro'})
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(`API connected to MongoDB & listening on port ${process.env.PORT}...`);
