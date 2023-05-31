5f729806db907aee09c6b564fe70f1be5bd4b057
alina <alina_2000@hotmail.co.uk>
2023-05-31 16:56:53 +0100

    added Quizpage export

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,6 +1,10 @@
 export { default as NavBar } from './NavBar';
 export { default as PomodoroTimer } from './PomodoroTimer';
 export { default as Flashcard } from './Flashcard';
+export {default as QuizSubjectCard} from './QuizSubjectCard';
+
+
+
 
 
 
